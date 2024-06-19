42c52cc367c81d8ce1e05f395afb4fb081860797
mike-menyala <159994059+mike-menyala@users.noreply.github.com>
2024-06-19 13:16:29 +0800

    commit message for multiple files

--- packages/hardhat/contracts/NFTMarketplace.sol
+++ packages/hardhat/contracts/NFTMarketplace.sol
@@ -13,9 +13,13 @@ contract NFTMarketplace is ERC721Enumerable, Ownable {
 
     mapping(uint256 => uint256) private _itemPrices;
     mapping(uint256 => address) private _itemSellers;
-
+    mapping(uint256 => address) private _itemBuyers;
+    mapping(uint256 => bool) private _itemSold;
+    mapping(uint256 => bool) private _itemReceived;
+    
     event ItemListed(uint256 indexed itemId, uint256 price, address seller);
     event ItemSold(uint256 indexed itemId, uint256 price, address buyer);
+    event ItemReceived(uint256 indexed itemId, address buyer);
 
     constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
 
@@ -30,16 +34,26 @@ contract NFTMarketplace is ERC721Enumerable, Ownable {
 
     function buyItem(uint256 itemId) public payable {
         require(_exists(itemId), "Item does not exist");
+        require(!_itemSold[itemId], "Item is already sold");
         require(msg.value >= _itemPrices[itemId], "Insufficient funds");
         
         address seller = _itemSellers[itemId];
-        transferFrom(seller, msg.sender, itemId);
-        _itemSellers[itemId] = address(0);
-        _itemPrices[itemId] = 0;
-        _itemsSold.increment();
+        _itemBuyers[itemId] = msg.sender;
+        _itemSold[itemId] = true;
         
-        payable(seller).transfer(msg.value);
         emit ItemSold(itemId, msg.value, msg.sender);
+    }
+
+    function confirmReceived(uint256 itemId) public {
+        require(_exists(itemId), "Item does not exist");
+        require(_itemSold[itemId], "Item is not sold");
+        require(msg.sender == _itemBuyers[itemId], "Only the buyer can confirm receipt");
+        
+        _itemReceived[itemId] = true;
+        address seller = _itemSellers[itemId];
+        transferFrom(seller, _itemBuyers[itemId], itemId);
+        
+        emit ItemReceived(itemId, _itemBuyers[itemId]);
     }
 
     function getItemPrice(uint256 itemId) public view returns(uint256) {
